252554c92d8f85711befbbf3c861c9e9876bff80
petershivachi <58518220+petershivachi@users.noreply.github.com>
2021-05-26 15:14:55 +0300

    components generated and navbar set-up

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FilesComponent } from './components/files/files.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddFileComponent } from './components/add-file/add-file.component';
+import { EditFileComponent } from './components/edit-file/edit-file.component';
+import { FileDetailsComponent } from './components/file-details/file-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    FilesComponent,
+    SidebarComponent,
+    AddFileComponent,
+    EditFileComponent,
+    FileDetailsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
